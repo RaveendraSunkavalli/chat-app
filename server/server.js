3e58d6a963ff49ca3f351e3e67a7d901f630495c
Ravi <s.ravindra153@gmail.com>
2019-02-16 18:05:50 +0530

    modified error

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const path =require('path');
 const express=require('express');
-const port= process.env.port||3000
+const port= process.env.PORT||3000
 const app=express();
 const staticPath=path.join(__dirname,'../public')
 app.use(express.static(staticPath));

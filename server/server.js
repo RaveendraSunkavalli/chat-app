ffd512b114ce1337bf618228929a0015398903bd
Ravi <s.ravindra153@gmail.com>
2019-02-17 07:40:19 +0530

    emit new message

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const path =require('path');
 const http=require('http');
-const socketIO=require('socket.io');
+
 const express=require('express');
-const port= process.env.PORT||3000
 const app=express();
 var server=http.createServer(app);
+const socketIO=require('socket.io');
+
+
+const port= process.env.PORT||3000
+
+
 var io=socketIO(server);
+
 const staticPath=path.join(__dirname,'../public')
 
 app.use(express.static(staticPath));
@@ -15,18 +21,22 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',()=>{
         console.log('user disconnected')
     });
-    socket.on('createEmail',(newEmail)=>{
-        console.log("Create email",newEmail);
-    });
-    socket.emit('newMessage',{
-        from:"screen.r@gmail.com",
-        text:"vbskjbsdbkjs",
-        createdAt:new Date().getTime(),
-    });
+    // socket.on('createEmail',(newEmail)=>{
+    //     console.log("Create email",newEmail);
+    // });
+    // socket.emit('newMessage',{
+    //     from:"screen.r@gmail.com",
+    //     text:"vbskjbsdbkjs",
+    //     createdAt:new Date().getTime(),
+    // });
     socket.on('createMessage',(message)=>{
         console.log(message);
-        
-    })
+        io.emit('newMessage',{
+            from:message.from,
+            text:message.text,
+            createdAt:new Date().getTime(),
+        });
+    });
     
 })
 
